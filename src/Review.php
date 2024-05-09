997be1c7673832c91b5b2938af214cc37cfb56c3
Faruk Nasir <frknasir@yahoo.com>
2024-05-09 10:15:25 +0100

    Update Review.php

--- src/Review.php
+++ src/Review.php
@@ -12,7 +12,7 @@ abstract class Review extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $fillable = [
         'team_id',
